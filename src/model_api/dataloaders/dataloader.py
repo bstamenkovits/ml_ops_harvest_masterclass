e0bcc125b39e20cc05760c43f8ba3dc480b5ffe7
Bas de Kan <basdekan@Bass-MacBook-Pro.local>
2023-05-31 15:54:41 +0200

    update to dataloader

--- src/model_api/dataloaders/dataloader.py
+++ src/model_api/dataloaders/dataloader.py
@@ -1,5 +1,6 @@
 import os
 from typing import Any
+import logging
 import sqlite3
 from itertools import repeat
 
@@ -7,6 +8,8 @@ import pandas as pd
 from pydantic import BaseModel
 
 import tensorflow_datasets as tfds
+
+logger = logging.getLogger()
 
 
 class DataLoader(BaseModel):
@@ -19,14 +22,19 @@ class DataLoader(BaseModel):
             self.init_db_from_tfds()
 
     def init_db_from_tfds(self):
+        logger.info('Initializing database from tensorflow dataset.')
         movies, movies_info = tfds.load(name='movielens/100k-movies', with_info=True)
         ratings, ratings_info = tfds.load(name='movielens/100k-ratings', with_info=True)
         df_movies = tfds.as_dataframe(ds=movies['train'], ds_info=movies_info)
         df_movies['movie_id'] = df_movies['movie_id'].astype(int)
         df_movies['movie_title'] = df_movies['movie_title'].astype(str)
 
+        logger.info('Created movies table.')
+
         df_genres = pd.DataFrame({'genre_id': [i for i in range(len(movies_info.features['movie_genres'].names))],
                                   'genre_name': movies_info.features['movie_genres'].names})
+
+        logger.info('Created genres table.')
 
         df_users_ratings = tfds.as_dataframe(ds=ratings['train'], ds_info=ratings_info).drop(columns=['movie_genres',
                                                                                                       'movie_title'])
@@ -38,20 +46,28 @@ class DataLoader(BaseModel):
         df_users = df_users_ratings.loc[:, ['user_id', 'bucketized_user_age', 'raw_user_age', 'user_gender',
                                             'user_occupation_label', 'user_occupation_text',
                                             'user_zip_code']].drop_duplicates()
+        df_users['hashed_passwords'] = ''
+
+        logger.info('Created users table.')
 
         df_ratings = df_users_ratings.loc[:, ['user_id', 'movie_id', 'user_rating']].drop_duplicates()
         df_ratings['rating_id'] = df_ratings.index
 
+        logger.info('Created ratings table.')
+
         if not os.path.isdir(self.db_path.split('/')[0]):
             os.mkdir(self.db_path.split('/')[0])
 
+        logger.info('Writing tables to local sqlite database.')
         with sqlite3.connect(self.db_path) as conn:
             df_movies.to_sql('movies', conn, if_exists='replace', index=False)
             df_genres.to_sql('genres', conn, if_exists='replace', index=False)
             df_users.to_sql('users', conn, if_exists='replace', index=False)
             df_ratings.to_sql('ratings', conn, if_exists='replace', index=False)
 
-    def query_data(self, query: str, params: tuple[str] | None = None):
+        logger.info('Database initialization finished.')
+
+    def query_data(self, query: str, params: list[Any] | None = None):
         with sqlite3.connect(self.db_path) as conn:
             return pd.read_sql(query, conn, params=params)
 
@@ -61,9 +77,9 @@ class DataLoader(BaseModel):
     def random_sample_table(self, table: str, n: int):
         return self.query_data(query=f'select * from {table} order by random() limit {n};')
 
-    def query_on_col_value(self, table: str, col_name: str, col_value: str):
+    def query_on_col_value(self, table: str, col_name: str, col_value: Any):
         sql = f'select * from {table} where {col_name}=?'
-        return self.query_data(query=sql, params=(col_value,))
+        return self.query_data(query=sql, params=[col_value])
 
     def insert_data(self, table: str, data: dict[str, Any]):
         with sqlite3.connect(self.db_path) as conn:
